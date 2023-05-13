873f66732d8fd0721604693a5d8ac85006928941
surprise <franciscoggouveia@gmail.com>
2023-05-13 14:12:57 +0100

    Updated with JsonSerialize

--- flutter_continental/lib/widgets/login_confirm.dart
+++ flutter_continental/lib/widgets/login_confirm.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_continental/pages/welcome_page.dart';
 
+import 'package:http/http.dart' as http;
+
 class LoginConfirm extends StatefulWidget {
   bool? isResponsive;
   double? size;
@@ -17,11 +19,16 @@ class LoginConfirm extends StatefulWidget {
 class _LoginConfirmState extends State<LoginConfirm> {
   bool _pressed = false;
 
-  void _onPressed(){
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const WelcomePage())
-    );
+  void _onPressed() async{
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => const WelcomePage())
+    // );
+
+    var url = Uri.http("my-json-server.typicode.com", "/typicode/demo/posts");
+    var response = await http.get(url);
+    print("Response body ${response.body}");
+    
   }
   @override
   Widget build(BuildContext context) {
